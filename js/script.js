acdbacc8b4847bcc0b52210e8ea8bb059c1be3ac
epan <ericpan@gmail.com>
2012-12-13 04:18:58 -0800

    Fades out Get 5 Friends button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 $(function() {
 	$("button#go:submit").click(function() {
-		$("button#go:submit").hide("slow");
-		$(".refresh-me").html('<p>To play again, <a id="refresh">refresh this page</a>.</p>');
-	});
-	$("#refresh").click(function() {
-		location.reload();
+		$("button#go:submit").fadeOut(1000);
 	});
 });
