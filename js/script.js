4d450c7b09223554dc703430e8e134d731402b32
epan <ericpan@gmail.com>
2012-12-13 03:28:19 -0800

    Adds refresh jQuery link

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(function() {
 	$("button#go:submit").click(function() {
 		$("button#go:submit").hide("slow");
-		$(".refresh-me").html("<p>Refresh this page to play again.</p>");
-	})
+		$(".refresh-me").html('<p>To play again, <a id="refresh">refresh this page</a>.</p>');
+	});
+	$("#refresh").click(function() {
+		location.reload();
+	});
 });
